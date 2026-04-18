da6a2d91c6e1d1cc61808e5500939f5230ed5198
Luh-code <Lasse@hueffler.de>
2026-04-18 04:11:56 +0200

    improved camera movement further

--- entities/pickups/coin/coin.gd
+++ entities/pickups/coin/coin.gd
@@ -5,9 +5,12 @@ extends Node2D
 func _ready() -> void:
 	sprite.play()
 
+func remove_self() -> void:
+	get_parent().remove_child(self)
+
 func _on_pickup_area_body_entered(body: Node2D) -> void:
 	if typeof(body) == typeof(Player):
 		var p: Player = body
 		p.add_coin(1)
-		get_parent().remove_child(self)
+		call_deferred("remove_self")
 		queue_free()
--- entities/player/player_camera.gd
+++ entities/player/player_camera.gd
@@ -5,8 +5,8 @@ extends Node2D
 @export var follow_strength := 0.2
 @export var follow_margin := 1.0
 @export var base_zoom: float = 1.5
-@export var velocity_factor_lerp: float = 1.0
-@export var velocity_factor_zoom: float = 1.0
+@export var distance_factor_lerp: float = 1.0
+@export var distance_factor_zoom: float = 0.441
 @export var zoom_strength: float = 0.2
 @export var lookahead_factor: float = 2.0
 
@@ -14,30 +14,23 @@ func _ready() -> void:
 	camera.zoom = Vector2(base_zoom, base_zoom)
 
 func _process(delta: float) -> void:
-	#if camera.position.distance_to(leader.position) > follow_margin:
-	#var goal = leader.position
-	var vel_factor: float = 0.0
-	
 	var dist = camera.position.distance_to(leader.position)
 	var dir = camera.position.direction_to(leader.position)
 	
 	var goal = dir * dist * lookahead_factor
+	goal = leader.position.move_toward(goal, follow_margin)
 	var dist_to_goal = camera.position.distance_to(goal)
 	
-	goal = leader.position.move_toward(goal, follow_margin)
+	# camera position
+	var dist_factor = dist_to_goal * distance_factor_lerp * delta
 	
-	if typeof(leader) == typeof(Player):
-		var p: Player = leader
-		vel_factor = dist_to_goal * velocity_factor_lerp * delta
-		
-		var goal_zoom: float = base_zoom
-		goal_zoom -= dist * velocity_factor_zoom / 500
-		
-		var zoom_delta = camera.zoom.x - goal_zoom
-		#zoom_delta *= velocity_factor_zoom
-		
-		var zoom_lerp = zoom_delta * zoom_strength * delta
-		
-		camera.zoom -= Vector2(zoom_lerp, zoom_lerp) 
+	camera.position = camera.position.lerp(goal, (follow_strength + dist_factor) * delta)
 	
-	camera.position = camera.position.lerp(goal, (follow_strength + vel_factor) * delta)
+	# camera zoom
+	var goal_zoom: float = base_zoom
+	goal_zoom -= dist_to_goal * distance_factor_zoom / 500
+	var zoom_delta = camera.zoom.x - goal_zoom
+	var zoom_lerp = zoom_delta * zoom_strength * delta
+	
+	camera.zoom -= Vector2(zoom_lerp, zoom_lerp) 
+	
